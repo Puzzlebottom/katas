3341c0840e99e4fe35e254f24f3b02d11bc8ee39
Conor Meldrum <conor.j.meldrum@gmail.com>
2023-06-29 22:30:24 -0400

    refactors for clarity

--- sum-largest-numbers.js
+++ sum-largest-numbers.js
@@ -1,7 +1,7 @@
-const sumLargestNumbers = (array) => array
+const sumLargestTwoNumbers = (array) => array
   .sort((a, b) => Number(b) - Number(a))
   .splice(0, 2)
   .reduce((a, b) => a + b);
   
 let test = [1, 4, 6, 2, 3, 1, 10, 2, 14];
-console.log(sumLargestNumbers(test));
+console.log(sumLargestTwoNumbers(test)); // => 24
